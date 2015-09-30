d4a27c65c18f1529b5e37b1bd0883f561f8a5ffa
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2015-10-01 02:50:29 +0600

    Made a tutorial screen.

--- app/src/main/java/com/prognosticator/funme/TutorialActivity.java
+++ app/src/main/java/com/prognosticator/funme/TutorialActivity.java
@@ -1,30 +1,22 @@
 package com.prognosticator.funme;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class TutorialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    /** Called when the user clicks the Tutorial button */
-    public void goToTutorial(View view) {
-        Intent intent = new Intent(this, TutorialActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_tutorial);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
         return true;
     }
 
