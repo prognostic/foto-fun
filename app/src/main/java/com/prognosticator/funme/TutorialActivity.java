55f9fe2ffd103243bb1a980f784121d18a95a38a
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2015-10-09 08:43:10 +0600

    Remove the menu on-screen tutorial.

--- app/src/main/java/com/prognosticator/funme/TutorialActivity.java
+++ app/src/main/java/com/prognosticator/funme/TutorialActivity.java
@@ -1,10 +1,8 @@
 package com.prognosticator.funme;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.google.android.gms.ads.AdRequest;
@@ -29,25 +27,4 @@ public class TutorialActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
