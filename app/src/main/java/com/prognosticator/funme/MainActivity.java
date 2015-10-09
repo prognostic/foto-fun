525e1c8d2fb46b0d41018cce7c860bc0a1e6d37a
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2015-10-09 11:57:37 +0600

    Remove the menu on welcome screen.

--- app/src/main/java/com/prognosticator/funme/MainActivity.java
+++ app/src/main/java/com/prognosticator/funme/MainActivity.java
@@ -3,8 +3,6 @@ package com.prognosticator.funme;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.gms.ads.AdRequest;
@@ -21,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        // display an icon in the Action bar
+        // ActionBar actionBar = getSupportActionBar();
+        // actionBar.setLogo(R.drawable.top_bar_logo);
+        // actionBar.setDisplayUseLogoEnabled(true);
+        // actionBar.setDisplayShowHomeEnabled(true);
     }
 
     /** Called when the user clicks the Tutorial button */
@@ -35,25 +38,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
