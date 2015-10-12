df520d91e26e569bef4c47d0f0002babe4ad835d
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2015-10-12 10:01:51 +0600

    Add activiti to the resulting fan.

---
+++ app/src/main/java/com/prognosticator/funme/TheResultingFanActivity.java
@@ -0,0 +1,13 @@
+package com.prognosticator.funme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TheResultingFanActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_resulting_fan);
+    }
+}
